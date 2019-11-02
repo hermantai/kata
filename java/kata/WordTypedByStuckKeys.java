33ad55edebeafe62646eaae7ef6d7deeb68fb064
Herman Tai <htaihm@gmail.com>
2019-11-02 13:31:42 -0700

    [java][WordTypedByStuckKeys] alternative solution completed

--- java/kata/WordTypedByStuckKeys.java
+++ java/kata/WordTypedByStuckKeys.java
@@ -11,11 +11,72 @@ import java.util.*;
  */
 public class WordTypedByStuckKeys {
   public static List<String> DICT = List.of(
-        "hello", "world", "cat", "dog", "bird", "green", "help", "great", "greet", "pool");
+        "hello", "world", "cat", "dog", "bird", "green", "help", "great", "greet", "pool", "leet", "let");
 
   static boolean wordTypedByStuckKeys(String str, List<String> dict) {
     TrieNode trie = TrieNode.build(dict);
     return trie.isInTrieForStucked(str);
+  }
+
+  /**
+   * Stores a map of Map<Character, List<String>words> instead of a Trie
+   */
+  static boolean wordTypedByStuckKeysWithMap(String str, List<String> dict) {
+    Map<String, List<String>> simplified = buildMap(dict);
+
+    String simplifiedString = simplifyString(str);
+    List<String> matcheds = simplified.get(simplifiedString);
+    if (matcheds == null) {
+      return false;
+    }
+    char[] chars = str.toCharArray();
+    for (String matched: matcheds) { 
+      if (matchedSimplified(chars, 0, matched.toCharArray(), 0)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static Map<String, List<String>>  buildMap(List<String> dict) {
+    Map<String, List<String>> simplified = new HashMap<>();
+    for (String s: dict) {
+      String newS = simplifyString(s);
+      List<String> values = simplified.get(newS);
+      if (values == null) {
+        values = new ArrayList<>();
+        simplified.put(newS, values);
+      }
+      values.add(s);
+    }
+    return simplified;
+  }
+
+  static String simplifyString(String s) {
+      int w = 1;
+      char[] chars = s.toCharArray();
+      for (int r = 1; r < chars.length; r++) {
+        if (chars[r] != chars[r - 1]) {
+          chars[w] = chars[r];
+          w++;
+        }
+      }
+      return new String(chars, 0, w);
+  }
+
+  static boolean matchedSimplified(char[] chars, int i, char[] tchars, int j) {
+    if (i == chars.length && j == tchars.length) {
+      return true;
+    }
+    if (i == chars.length || j == tchars.length) {
+      return false;
+    }
+
+    if (chars[i] != tchars[j]) {
+      return false;
+    }
+    return matchedSimplified(chars, i + 1, tchars, j) || matchedSimplified(
+        chars, i + 1, tchars, j + 1);
   }
 
   public static void main(String args[]) {
@@ -27,10 +88,12 @@ public class WordTypedByStuckKeys {
     runSample("green", true);
     runSample("aaaata", false);
     runSample("poolllll", true);
+    runSample("leeet", true);
+    runSample("let", true);
   }
 
   static void runSample(String s, boolean expected) {
-    System.out.printf("%s = %s (expected: %s)\n", s, wordTypedByStuckKeys(s, DICT), expected);
+    System.out.printf("%s = %s and %s (expected: %s)\n", s, wordTypedByStuckKeys(s, DICT), wordTypedByStuckKeysWithMap(s, DICT), expected);
   }
 
   static class TrieNode {
