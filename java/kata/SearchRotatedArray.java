c9b3bee850d777b300ac26f5bc306ca93ac2c4c2
Herman Tai <htaihm@gmail.com>
2019-12-04 20:44:07 -0800

    minor touchup

--- java/kata/SearchRotatedArray.java
+++ java/kata/SearchRotatedArray.java
@@ -7,6 +7,8 @@ import java.util.*;
  * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
  * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
  * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ *
+ * <p>https://leetcode.com/problems/search-in-rotated-sorted-array/
  */
 public class SearchRotatedArray {
 	static int searchRotatedArray(int[] nums, int target) {
